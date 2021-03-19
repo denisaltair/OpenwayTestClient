694cf397745f3e2a22ebde128b0071de95f520c2
altair83 <sital@list.ru>
2021-03-19 17:34:03 +0600

    Доделал тест D25

--- src/test/java/OpenwayTests/pos_test/D25_I17_PurchaseWithCashBack.kt
+++ src/test/java/OpenwayTests/pos_test/D25_I17_PurchaseWithCashBack.kt
@@ -30,7 +30,9 @@ class D25_I17_PurchaseWithCashBack : TestCase() {
             cashBackAmount = BigDecimal(10),
             cardHolderVerificationType = SIGNED,
             description = "Purchase with cashback. Operation should be rejected by terminal. ",
-            cardSlotType = ICC
+            cardSlotType = ICC,
+            currency = RUB
+
         )
         println(testResult.resultMessage)
         assertEquals(testResult.openwayResponseCode, UNKNOWN_ERROR)
@@ -44,7 +46,8 @@ class D25_I17_PurchaseWithCashBack : TestCase() {
             cashBackAmount = BigDecimal(10),
             cardHolderVerificationType = SIGNED,
             description = "Purchase with cashback. Operation should be rejected by terminal. ",
-            cardSlotType = ICC
+            cardSlotType = ICC,
+            currency = USD
         )
         println(testResult.resultMessage)
         assertEquals(testResult.openwayResponseCode, UNKNOWN_ERROR)
@@ -58,11 +61,36 @@ class D25_I17_PurchaseWithCashBack : TestCase() {
             cashBackAmount = BigDecimal(10),
             cardHolderVerificationType = SIGNED,
             description = "Purchase with cashback.",
-            cardSlotType = ICC
+            cardSlotType = ICC,
+            currency = RUB
         )
         println(testResult.resultMessage)
         assertEquals(testResult.openwayResponseCode, ACCEPTED)
 
+//--------------------------------------------------------------
+        testResult = EmvCardsTesterHelper.sendRequest(
+            testNumber = "DB25.04",
+            testCard = EMV_10,
+            operationType = PURCHASE,
+            amount = BigDecimal(2504.54),
+            cashBackAmount = BigDecimal(10),
+            cardHolderVerificationType = SIGNED,
+            description = "Purchase with cashback.",
+            cardSlotType = ICC,
+            currency = USD
+
+        )
+        println(testResult.resultMessage)
+        assertEquals(testResult.openwayResponseCode, ACCEPTED)
+
+//----------------------------------------------------
+        testResult = EmvCardsTesterHelper.sendRequest(
+            operationType = RECONCILIATION,
+            testNumber = "D25.05",
+            description = "Reconciliation"
+        )
+        println(testResult.resultMessage)
+        assertEquals(testResult.openwayResponseCode, ACCEPTED)
 
     }
 
